83d477661a49c345a0823c94f26348488c0bcd52
EsIstJosh <joshjconkin@gmail.com>
2025-05-15 16:46:47 -0400

    3.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='lightweight_charts_esistjosh',
-    version='3.0.7',
+    version='3.0.8',
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=[
